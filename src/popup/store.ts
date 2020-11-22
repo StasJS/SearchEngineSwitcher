99c67343e294625148af4b7e2b670cfda12a40e7
Jordan Schroder <willferrell.rules@gmail.com>
2020-11-22 13:27:36 +1300

    Saving state to local storage

--- src/popup/store.ts
+++ src/popup/store.ts
@@ -1,9 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import rootReducer from "./rootReducer";
+import { save, load } from "redux-localstorage-simple";
 
 const store = configureStore({
-	reducer: rootReducer
+	reducer: rootReducer,
+	middleware: (d) => d().concat(save()),
+	preloadedState: load()
 });
 
 export default store;
