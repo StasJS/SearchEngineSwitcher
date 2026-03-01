2e12522840ce72ad62a3096bea6b67429363d88f
StasJS <StasJS@users.noreply.github.com>
2026-03-01 07:59:53 +0000

    deploy: 3ae0350913a26c45c42554786df9823beb7d6288

--- chrome/assets/js/background.bundle.js
+++ chrome/assets/js/background.bundle.js
@@ -1,1 +1,1 @@
-import{e as t,f as e}from"./storage-VMRlgmV-.chunk.js";t.runtime.onInstalled.addListener(async()=>{const a=await e();await t.storage.local.set(a)});
+import{e as t,f as e}from"./storage-B2DC6Eph.chunk.js";t.runtime.onInstalled.addListener(async()=>{const a=await e();await t.storage.local.set(a)});
